63862ffb68b1827a8d303d132ce11814f409fe00
simology <simohammed.latifi@gmail.com>
2023-02-27 22:04:39 +0100

    dir ok

--- config/routes.php
+++ config/routes.php
@@ -6,5 +6,5 @@ return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class);
 	$app->get('/home', \App\Controllers\Home::class . ':index');
 	$app->get('/welcome', \App\Controllers\Home::class . ':getUser');
-	//$app->get('/users', \App\Controllers\Home::class . ':getUsers');
+	$app->get('/users', \App\Controllers\Home::class . ':getUser');
 };
